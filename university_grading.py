d558c6dd1debd0be1367f70c02b585741ec1b48d
apupaul-ai <apupaul7666100@gmail.com>
2026-07-18 23:59:38 +0600

    Day 2: Added view student feature with all subjects

--- university_grading.py
+++ university_grading.py
@@ -14,16 +14,31 @@ def add_student():
     print(f"{name} added successfully with {len(students[name])} subject(s)!")
 
 
+def view_student():
+    name = input("Enter student name to view: ")
+
+    if name not in students:
+        print("Student not found.")
+        return
+
+    print(f"\n--- Record for {name} ---")
+    for subject, marks in students[name].items():
+        print(f"{subject}: {marks}")
+
+
 while True:
     print("\n--- University Grading System ---")
     print("1. Add Student")
-    print("2. Exit")
+    print("2. View Student")
+    print("3. Exit")
 
-    choice = input("Choose an option (1-2): ")
+    choice = input("Choose an option (1-3): ")
 
     if choice == "1":
         add_student()
     elif choice == "2":
+        view_student()
+    elif choice == "3":
         print("Thank you!")
         break
     else:
